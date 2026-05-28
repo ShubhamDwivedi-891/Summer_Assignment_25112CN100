d30aba7f271e7be82e283019b0c40592398c9a96
ShubhamDwivedi-891 <shubhamdwivedi6984@gmail.com>
2026-05-28 10:58:44 +0530

    Added Question.1 program

---
+++ Question.1.c
@@ -0,0 +1,17 @@
+//Write a program to calculate sum of first N natural numbers.
+
+#include<stdio.h>
+int main()
+{
+    int N, i, sum=0 ;
+    printf("Enter the value of N :");
+    scanf("%d", &N);
+    
+    for(i=1; i<=N; i++){
+        sum = sum + i ;
+    }
+    
+    printf("The sum of first %d natural numbers is %d", N, sum);
+    return 0;
+}
+    
